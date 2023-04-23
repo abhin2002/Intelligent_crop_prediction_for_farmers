0c264c0cafea1a83e0d86200dc5c997c11e54141
abhirajp075@gmail.com <abhirajp075@gmail.com>
2023-04-23 12:50:38 +0530

    integration done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import SignUp from './Pages/signUp';
 import SignIn from './Pages/signIn';
+import HomePrediction from './Pages/Homepage';
 
 function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/');
+            const jsonData = await response.json();
+            setData(jsonData);
+        }
+        fetchData();
+    }, []);
+
     return (
         <ChakraProvider>
-        <Router>
-            <Switch>
-            <Route path="/signup">
-                <SignUp></SignUp>
-            </Route>
-            <Route path="/signin">
-                <SignIn></SignIn>
-            </Route>
-            </Switch>
-        </Router>
+            <Router>
+                <Switch>
+                    <Route path="/signup">
+                        <SignUp></SignUp>
+                    </Route>
+                    <Route path="/signin">
+                        <SignIn></SignIn>
+                    </Route>
+                    <Route path="/">
+                        <HomePrediction data={data}></HomePrediction> {/* pass the data to the component */}
+                    </Route>
+                </Switch>
+            </Router>
         </ChakraProvider>
     );
 }
--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,14 +1,62 @@
-import { Box, Container, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
 
-const Homepage = () => {
-    return(
-        <Container maxW = 'xl' centerContent>
-            <Box className='TitleBox'>
-                <Text className='heading'>Intelligent Crop Analyser</Text>
-            </Box>
-        </Container>
-        )   
+// nitrogen 
+// pottassium
+// phosphorous
+// ph
+// rainfall
+// humidity
+
+function HomePrediction() {
+    const [nitrogen, setNitrogen] = useState('');
+    const [pottassium, setPottassium] = useState('');
+    const [phosphorous, setPhosphorous] = useState('');
+    const [ph, setPh] = useState('');
+    const [rain, setRain] = useState('');
+    const [humidity, setHumid] = useState('');
+
+
+    function handleSubmit(e) {
+        // logic for handling sign in form submission goes here
+        e.preventDefault();
+        console.log(nitrogen,pottassium,phosphorous,ph,rain,humidity);
+    }
+
+    return (
+        <Box p={4}>
+        <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+            <FormControl>
+                <FormLabel>Nitrogen</FormLabel>
+                <Input type="number" value={nitrogen} onChange={(e) => setNitrogen(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>Pottassium</FormLabel>
+                <Input type="number" value={pottassium} onChange={(e) => setPottassium(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>Phosphorous</FormLabel>
+                <Input type="number" value={phosphorous} onChange={(e) => setPhosphorous(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>ph</FormLabel>
+                <Input type="number" value={ph} onChange={(e) => setPh(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>Rainfall</FormLabel>
+                <Input type="number" value={rain} onChange={(e) => setRain(e.target.value)} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>Humidity</FormLabel>
+                <Input type="number" value={humidity} onChange={(e) => setHumid(e.target.value)} />
+            </FormControl>
+            <Button type="submit" colorScheme="blue">Show results</Button>
+            </Stack>
+        </form>
+        </Box>
+    );
 }
 
-export default Homepage
+export default HomePrediction;
